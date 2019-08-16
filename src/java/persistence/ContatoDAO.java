b625fd0356df7a612b450376813c3da4092f3b7b
joaopsjc <pedrocxm@gmail.com>
2019-08-15 21:14:58 -0300

    dia 15 de agosto

---
+++ src/java/persistence/ContatoDAO.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package persistence;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author ice
+ */
+public class ContatoDAO {
+    private static ContatoDAO instance = new ContatoDAO();
+    public static ContatoDAO getInstance()
+    {
+        return instance;
+    }
+    
+    public void save(String nome, String email) throws SQLException,ClassNotFoundException
+    {
+        Connection conn = null;
+        Statement st = null;
+        try {
+            conn = DatabaseLocator.getInstance().getConnection();
+            st = conn.createStatement();
+            st.execute("insert into contato (nome, email)" +
+                       " values ('" + nome + "', '" + email + "')");
+        } catch (SQLException e) {
+            throw e;
+        } 
+        finally
+        {
+            closeResources(conn,st);
+        }
+    }
+
+    private void closeResources(Connection conn, Statement st) {
+        try {
+                    if(st!=null) st.close();
+                    if(conn!=null) conn.close();
+                    
+                } catch(SQLException e) {
+                    
+                }
+    }
+}
